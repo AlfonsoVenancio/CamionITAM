691a4fe229e3680d817e110ec5bb5faf89d6a6ea
Alfonso Venancio <alfonso.venancio22@gmail.com>
2018-08-27 19:37:43 -0500

    Creacion de interfaz para registro de notificaciones

--- app/src/main/java/venac/io/camionitam/Fragmentos/creacionNotificacion.java
+++ app/src/main/java/venac/io/camionitam/Fragmentos/creacionNotificacion.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import venac.io.camionitam.R;
 
@@ -24,7 +26,12 @@ public class creacionNotificacion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_creacion_notificacion, container, false);
+        View v = inflater.inflate(R.layout.fragment_creacion_notificacion, container, false);
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(v.getContext(),R.array.horarios_metro,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        return v;
     }
 
 }
